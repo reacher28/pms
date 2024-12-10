b69ebe8f8985cc0d0ac8f6a1fdb77c217f5a69ec
Saniev Ruslan <sanrustiK@yandex.ru>
2024-12-10 03:47:53 +0300

    First commit

---
+++ app/src/main/java/com/example/pms_labs/greeting.kt
@@ -0,0 +1,10 @@
+package com.example.pms_labs
+
+fun main() {
+    birthdayGreeting()
+}
+
+fun birthdayGreeting(): Unit {
+    println("Happy Birthday, Rover!")
+    println("You are now 5 years old!")
+}
