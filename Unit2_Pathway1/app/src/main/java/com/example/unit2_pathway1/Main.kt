f78453b3757ecb15c80230ecd6eef3e476c8d088
Saniev Ruslan <sanrustiK@yandex.ru>
2024-12-25 02:33:06 +0300

    nullability

--- Unit2_Pathway1/app/src/main/java/com/example/unit2_pathway1/Main.kt
+++ Unit2_Pathway1/app/src/main/java/com/example/unit2_pathway1/Main.kt
@@ -1,71 +1,56 @@
 package com.example.unit2_pathway1
 
 fun main() {
-    funcIf()
-    funcWhen()
-    funcWhenNumbers()
-    funcNewIf()
-    funcNewWhen()
-
+    nullOrString()
+    nullorInt()
+    lengthIfNull()
+    legnthIfNotNull()
+    usingIfElse()
+    elvis()
 }
 
-fun funcIf(){
-    val trafficLightColor = "Black"
+fun nullOrString() {
+    var favoriteActor: String? = "Sandra Oh"
+    println(favoriteActor)
 
-    if (trafficLightColor == "Red") {
-        println("Stop")
-    } else if (trafficLightColor == "Yellow") {
-        println("Slow")
-    } else if (trafficLightColor == "Green") {
-        println("Go")
+    favoriteActor = null
+    println(favoriteActor)
+}
+
+fun nullorInt() {
+    var number: Int? = 10
+    println(number)
+
+    number = null
+    println(number)
+}
+
+fun lengthIfNull() {
+    var favoriteActor: String? = "Sandra Oh"
+    println(favoriteActor?.length)
+}
+
+fun legnthIfNotNull() {
+    var favoriteActor: String? = "Sandra Oh"
+    println(favoriteActor!!.length)
+}
+
+fun usingIfElse() {
+    var favoriteActor: String? = "Sandra Oh"
+
+    val lengthOfName = if (favoriteActor != null) {
+        favoriteActor.length
     } else {
-        println("Invalid traffic-light color")
+        0
     }
 
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
 
-fun funcWhen(){
-    val trafficLightColor = "Amber"
+fun elvis() {
+    var favoriteActor: String? = "Sandra Oh"
 
-    when (trafficLightColor) {
-        "Red" -> println("Stop")
-        "Yellow", "Amber" -> println("Slow")
-        "Green" -> println("Go")
-        else -> println("Invalid traffic-light color")
-    }
-}
+    val lengthOfName = favoriteActor?.length ?: 0
 
-fun funcWhenNumbers() {
-    val x: Any = 20
-
-    when (x) {
-        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
-        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
-        is Int -> println("x is an integer number, but not between 1 and 10.")
-        else -> println("x isn't an integer number.")
-    }
-}
-
-fun funcNewIf(){
-    val trafficLightColor = "Black"
-
-    val message =
-        if (trafficLightColor == "Red") "Stop"
-        else if (trafficLightColor == "Yellow") "Slow"
-        else if (trafficLightColor == "Green") "Go"
-        else "Invalid traffic-light color"
-
-    println(message)
-}
-
-fun funcNewWhen() {
-    val trafficLightColor = "Amber"
-
-    val message = when(trafficLightColor) {
-        "Red" -> "Stop"
-        "Yellow", "Amber" -> "Slow"
-        "Green" -> "Go"
-        else -> "Invalid traffic-light color"
-    }
-    println(message)
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
