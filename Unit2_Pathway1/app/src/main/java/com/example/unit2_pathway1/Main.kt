9f2b6ba7b1a8d709c86c87ad907ff844fc2baadb
Saniev Ruslan <sanrustiK@yandex.ru>
2024-12-25 02:19:44 +0300

    Conditionals

---
+++ Unit2_Pathway1/app/src/main/java/com/example/unit2_pathway1/Main.kt
@@ -0,0 +1,71 @@
+package com.example.unit2_pathway1
+
+fun main() {
+    funcIf()
+    funcWhen()
+    funcWhenNumbers()
+    funcNewIf()
+    funcNewWhen()
+
+}
+
+fun funcIf(){
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+
+}
+
+fun funcWhen(){
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+
+fun funcWhenNumbers() {
+    val x: Any = 20
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
+
+fun funcNewIf(){
+    val trafficLightColor = "Black"
+
+    val message =
+        if (trafficLightColor == "Red") "Stop"
+        else if (trafficLightColor == "Yellow") "Slow"
+        else if (trafficLightColor == "Green") "Go"
+        else "Invalid traffic-light color"
+
+    println(message)
+}
+
+fun funcNewWhen() {
+    val trafficLightColor = "Amber"
+
+    val message = when(trafficLightColor) {
+        "Red" -> "Stop"
+        "Yellow", "Amber" -> "Slow"
+        "Green" -> "Go"
+        else -> "Invalid traffic-light color"
+    }
+    println(message)
+}
